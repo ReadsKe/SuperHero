20551fed229235b4a0bc932e458a884cc4c24411
Samuel Kimote <readssamuel@.com>
2024-01-16 10:07:56 +0300

    first commit

---
+++ code-challange-wk3/python-code-challenge-superheroes/code-challenge/app/models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Hero(db.Model):
+    __tablename__ = 'hero'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    super_name = db.Column(db.String(100), nullable=False)
+    powers = db.relationship('Power', secondary='hero_power', backref='heroes')
+
+class Power(db.Model):
+    __tablename__ = 'power'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+class HeroPower(db.Model):
+    __tablename__ = 'hero_power'
+    id = db.Column(db.Integer, primary_key=True)
+    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'), nullable=False)
+    power_id = db.Column(db.Integer, db.ForeignKey('power.id'), nullable=False)
+    strength = db.Column(db.String(100), nullable=False)
+
+    hero = db.relationship('Hero', backref=db.backref('hero_powers', cascade='all, delete-orphan'))
+    power = db.relationship('Power')
---
+++ code-challange-wk3/python-code-challenge-superheroes/code-challenge/client/src/components/App.js
@@ -0,0 +1,36 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import Hero from "./Hero";
+import Home from "./Home";
+import HeroPowerForm from "./HeroPowerForm";
+import Power from "./Power";
+import PowerEditForm from "./PowerEditForm";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Routes>
+          <Route exact path="/hero_powers/new">
+            <HeroPowerForm />
+          </Route>
+          <Route exact path="/powers/:id/edit">
+            <PowerEditForm/>
+          </Route>
+          <Route exact path="/powers/:id">
+            <Power />
+          </Route>
+          <Route exact path="/heroes/:id">
+            <Hero />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
